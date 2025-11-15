3366d40b02843a83ac047205dda34f5b74798a66
sscods <aasishsharma99911@gmail.com>
2025-11-15 21:17:11 +0530

    Implemented CQRS for Endpoints

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create Product entity from cmd object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+            //save to database
+
+            //return CreateProductResult
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 //Configure the HTTP requesr pipeline.
+app.MapCarter();
 
 app.Run();
