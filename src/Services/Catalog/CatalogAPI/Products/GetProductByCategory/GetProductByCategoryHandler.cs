fa5568cf628de809386aab8a1cfcf999b1553f1e
sscods <aasishsharma99911@gmail.com>
2025-11-19 14:50:42 +0530

    Completed All Endpoints for Catelog microservice

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,25 @@
+﻿
+using CatalogAPI.Products.GetProductById;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogAPI.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) :
+        IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    public class GetProductByCategoryQueryHandler (IDocumentSession session , ILogger<GetProductByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@query}", query);
+
+            var product = await session.Query<Product>()
+                                       .Where(p => p.Category.Contains(query.Category))
+                                       .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(product);           
+        }
+    }
+}
