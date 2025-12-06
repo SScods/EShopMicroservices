2e14f5a4234d48cd3acc99fa1beb08e698d344e7
sscods <aasishsharma99911@gmail.com>
2025-12-06 13:30:38 +0530

    Completed basket microservices using decorator pattern with redis distrivuted cache

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Basket.API.Basket.GetBasket;
+
+namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart);
+    public record StoreBasketResponse(string UserName);
+    public class StoreBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+                var result = await sender.Send(command);
+                var resonse = result.Adapt<StoreBasketResponse>();
+
+                return Results.Created($"/basket/{resonse.UserName}", resonse);
+            })
+            .WithName("CreateProduct")
+            .Produces<GetBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,39 +1,40 @@
-
+using BuildingBlocks.Exceptions.Handler;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add Services to the container.
-
+//Add services to the container.
 var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-builder.Services.AddValidatorsFromAssembly(assembly);
-
-builder.Services.AddCarter();
-
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x=>x.UserName);
 }).UseLightweightSessions();
 
-if (builder.Environment.IsDevelopment())
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
 {
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
-}
-
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
-//Configure the HTTP requesr pipeline.
+//Configure the HTTP reqhuest pipeline
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 app.UseHealthChecks("/health", new HealthCheckOptions
